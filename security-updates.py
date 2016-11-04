dc03bce8db55d2158663f6e2d57ee16e860c45fd
Andrew Thornton <andy.thornton@gmail.com>
2016-11-04 11:24:17 -0400

    Cleaned up output (again)

--- security-updates.py
+++ security-updates.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import subprocess
 
-#Store paths and connection info
+# Store paths and connection info
 sourcetree = "/home/flux/Projects/hackday"
 gitlabapi = ""
 drupalbootstrap = sourcetree + "/drupal/includes/bootstrap.inc"
 rssDrupalCore = "http://drupal.org/security/rss.xml"
 rssDrupalContrib = "http://drupal.org/security/contrib/rss.xml"
 
-#Start with Drupal core
-print("\033[1;34;40m") + "======= Drupal Core ========\033[0;37;40m"
+# Start with Drupal core
+print("=" * 7) + " Drupal Core " + ("=" * 7)
 with open(drupalbootstrap, 'r') as searchfile:
     for line in searchfile:
         if """define('VERSION',""" in line:
             drupalversion =  line.split("'")
-            print "Drupal Core = " + drupalversion[3]
+            print "-- Drupal Core: " + drupalversion[3]
 
-#Iterate modules ignoring core.
+# Iterate modules ignoring core.
 print("=" * 7) + " Contributed Modules " + ("=" * 7)
+
+# Start with the drupal /modules directory as this is where RPM's install
+# Exclude all files with package "Core"
+rpmmodules = sourcetree + "drupal/modules"
+print("--- Scanning " + rpmmodules + " directory for non core modules")
