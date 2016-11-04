99a24eba3822986e030d7fe641371064f58f816e
Andrew Thornton <andy.thornton@gmail.com>
2016-11-04 15:02:59 -0400

    added contrib folder

--- security-updates.py
+++ security-updates.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 import subprocess, os, sys
+import colorama
+from colorama import Fore, Back, Style
 
 # Store paths and connection info
 sourcetree = "/home/flux/Projects/hackday"
 gitlabapi = ""
 drupalbootstrap = sourcetree + "/drupal/includes/bootstrap.inc"
+contrib = sourcetree + "/all/modules/contrib"
 rssDrupalCore = "http://drupal.org/security/rss.xml"
 rssDrupalContrib = "http://drupal.org/security/contrib/rss.xml"
 
 # Start with Drupal core
-print("=" * 7) + " Drupal Core " + ("=" * 7)
+print Fore.BLUE + ("=" * 7) + " Drupal Core " + ("=" * 7) + Style.RESET_ALL
+print Fore.GREEN
 with open(drupalbootstrap, 'r') as searchfile:
     for line in searchfile:
         if """define('VERSION',""" in line:
             drupalversion =  line.split("'")
             print "-- Drupal Core: " + drupalversion[3]
+print Style.RESET_ALL
 
 # Iterate modules ignoring core.
-print("=" * 7) + " Contributed Modules " + ("=" * 7)
+print Fore.BLUE + ("=" * 7) + " Contributed Modules under /modules " + ("=" * 7) + Style.RESET_ALL
 
 # Start with the drupal /modules directory as this is where RPM's install
-# Exclude all files with package "Core"
+# TODO: Exclude all files with package "Core"
 rpmmodules = sourcetree + "/drupal/modules"
 dirs = os.listdir(rpmmodules)
+print Fore.GREEN
 for module in dirs:
     info = sourcetree + "/drupal/modules/" + module + "/" + module + ".info"
     try:
@@ -32,6 +38,25 @@ for module in dirs:
                     moduleversion = line.split("version =")
                     if not "VERSION" in moduleversion[1]:
                         if moduleversion[1] <> "VERSION":
-                            print "-- Module: " + module + " " + moduleversion[1]
+                            print "-- " + module + " " + moduleversion[1]
     except:
         pass
+print Style.RESET_ALL
+
+# Now look at the contrib folder
+print Fore.BLUE + ("=" * 7) + " Contributed Modules under /all/modules/contrib "  + ("=" * 7) + Style.RESET_ALL
+rpmmodules = contrib
+dirs = os.listdir(rpmmodules)
+print Fore.GREEN
+for module in dirs:
+    info = contrib  + "/" +  module + "/" + module + ".info"
+    try:
+        with open(info, 'r') as searchfile:
+            for line in searchfile:
+                if """version = """ in line:
+                    moduleversion = line.split("version =")
+                    if not "VERSION" in moduleversion[1]:
+                        print "-- " + module + " " + moduleversion[1]
+    except:
+        pass
+print Style.RESET_ALL
